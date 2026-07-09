2b553c37d704ac195786fc2f407ec05bef1067f6
dmkeijiban <akariri1706@yahoo.co.jp>
2026-07-09 16:04:27 +0900

    デュエマプロフィール回答の縦位置を中央寄せ
    Keep labels at the top while vertically centering answer text in each duema profile card.

--- src/app/u/[slug]/layout.tsx
+++ src/app/u/[slug]/layout.tsx
@@ -6,7 +6,17 @@ export default function PublicUserProfileLayout({ children }: { children: ReactN
       <style
         dangerouslySetInnerHTML={{
           __html: `
+            dl.grid.flex-1 > div {
+              display: flex;
+              flex-direction: column;
+            }
+
             dl.grid.flex-1 > div > dd {
+              flex: 1;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin-top: 0;
               text-align: center;
             }
           `,
