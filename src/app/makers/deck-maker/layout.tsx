57f57dc5e454c566fdc2a23148bf074d93de193d
dmkeijiban <akariri1706@yahoo.co.jp>
2026-07-19 13:17:48 +0900

    収録版の読み込み表示を短縮 (#656)

--- src/app/makers/deck-maker/layout.tsx
+++ src/app/makers/deck-maker/layout.tsx
@@ -8,6 +8,15 @@ export default function DeckMakerLayout({ children }: { children: ReactNode }) {
           display: none;
         }
 
+        [aria-labelledby="card-dialog-title"] .mt-5 > p:nth-child(2) {
+          font-size: 0;
+        }
+
+        [aria-labelledby="card-dialog-title"] .mt-5 > p:nth-child(2)::after {
+          content: '読み込み中…';
+          font-size: 0.75rem;
+        }
+
         [aria-labelledby="card-dialog-title"] .mt-5 > div > button > span {
           display: none;
         }
